2dec671d0709ebaa1eb0168d6e8f77a7c8f06390
robertogoz <gozjunior@hotmail.com>
2022-07-05 11:03:30 -0300

    feat(app): add tab navigation  and initial configs

--- food-app/src/routes/AppRoutes.tsx
+++ food-app/src/routes/AppRoutes.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import { HomeScreen } from '../screens/HomeScreen'
+import { MainRoutes } from './MainRoutes'
 
 const Stack = createNativeStackNavigator()
 
 export function AppRoutes() {
   return (
-    <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Navigator initialRouteName="Main" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Main" component={MainRoutes} />
     </Stack.Navigator>
   )
 }
---
+++ food-app/src/routes/MainRoutes.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons } from '@expo/vector-icons'
+
+import { HomeScreen } from '../screens/HomeScreen'
+import { ProfileScreen } from '../screens/ProfileScreen'
+
+const Tab = createBottomTabNavigator()
+
+export function MainRoutes() {
+  return (
+    <Tab.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: () => <Ionicons name="home-outline" size={24} />,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: () => <Ionicons name="person-outline" size={24} />,
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
