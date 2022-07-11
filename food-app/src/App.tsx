6874e869fe9776e85896a9ca51696c0bef6ff28b
robertogoz <gozjunior@hotmail.com>
2022-07-11 11:14:59 -0300

    chore(app): add react-query in project

--- food-app/src/App.tsx
+++ food-app/src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import { AuthProvider } from './contexts/auth'
+import { QueryClientProvider } from 'react-query'
 
+import { AuthProvider } from './contexts/auth'
+import { queryClient } from './services/queryClient'
 import { Routes } from './routes'
 
 export function App() {
   return (
-    <AuthProvider>
-      <NavigationContainer>
-        <Routes />
-      </NavigationContainer>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <NavigationContainer>
+          <Routes />
+        </NavigationContainer>
+      </AuthProvider>
+    </QueryClientProvider>
   )
 }
