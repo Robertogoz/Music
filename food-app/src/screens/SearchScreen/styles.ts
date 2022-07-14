f0610e3da3d617c66b2466b79ba641c11ab3b7b2
robertogoz <gozjunior@hotmail.com>
2022-07-14 14:15:57 -0300

    style(app): change search input position

--- food-app/src/screens/SearchScreen/styles.ts
+++ food-app/src/screens/SearchScreen/styles.ts
@@ -2,8 +2,9 @@ import styled from 'styled-components/native'
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
+  margin-top: 10%;
 `
 
 export const Title = styled.Text`
