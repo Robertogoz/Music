db5f179b2cdb487aacd29dce3b57dbf23175d043
robertogoz <gozjunior@hotmail.com>
2022-07-14 16:15:09 -0300

    feat(app): create search recommendations component

---
+++ food-app/src/screens/SearchScreen/components/SearchRecommendations/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { View, Text } from 'react-native'
+import { SearchRecommendationBox } from './styles'
+
+export function SearchRecommendations() {
+  return (
+    <SearchRecommendationBox>
+      <Text>Search Recommendations</Text>
+    </SearchRecommendationBox>
+  )
+}
--- food-app/src/screens/SearchScreen/index.tsx
+++ food-app/src/screens/SearchScreen/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Keyboard, TouchableWithoutFeedback } from 'react-native'
 
-import { Container } from './styles'
 import { SearchInput } from './components/SearchInput'
+import { SearchRecommendations } from './components/SearchRecommendations'
+import { Container } from './styles'
 
 export function SearchScreen() {
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
       <Container>
         <SearchInput />
+        <SearchRecommendations />
       </Container>
     </TouchableWithoutFeedback>
   )
