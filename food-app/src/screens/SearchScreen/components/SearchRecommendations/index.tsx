95fe81f974d30023c4d2d846fb58e32a6c465cd2
robertogoz <gozjunior@hotmail.com>
2022-07-18 09:29:14 -0300

    feat(app): add navigation to playlist details into searchRecommendations playlists

--- food-app/src/screens/SearchScreen/components/SearchRecommendations/index.tsx
+++ food-app/src/screens/SearchScreen/components/SearchRecommendations/index.tsx
@@ -1,21 +1,28 @@
+import { useNavigation } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import React, { useContext } from 'react'
 import { Platform } from 'react-native'
 
 import { useQuery } from 'react-query'
 import { SpotifyContext } from '../../../../contexts/spotify'
+import { RootStackAppRoutes } from '../../../../routes/AppRoutes'
 import { Recommendations } from '../../../../types/getRecommendationsType'
 import {
   SearchRecommendationBox,
   PlaylistList,
   PlaylistImage,
   PlaylistTitle,
+  PlaylistButton,
   RecommendationsTitle,
   PlaylistDescription,
   PlaylistData,
   Flat,
 } from './styles'
 
+type PlaylistScreenProps = NativeStackNavigationProp<RootStackAppRoutes, 'Main'>
+
 export function SearchRecommendations() {
+  const navigation = useNavigation<PlaylistScreenProps>()
   const { getRecommendations } = useContext(SpotifyContext)
   const { data } = useQuery<Recommendations>('recommendations', async () => await getRecommendations(), {
     staleTime: 1000 * 86400, // 1 day
@@ -23,11 +30,20 @@ export function SearchRecommendations() {
 
   const RenderItem = (item: any) => (
     <PlaylistList>
-      <PlaylistImage source={{ uri: item?.item.images[0].url }} />
-      <PlaylistData>
-        <PlaylistTitle>{item?.item.name}</PlaylistTitle>
-        <PlaylistDescription>{item?.item.description}</PlaylistDescription>
-      </PlaylistData>
+      <PlaylistButton
+        activeOpacity={0.5}
+        onPress={() =>
+          navigation.navigate('PlaylistDetails', {
+            id: item?.item.id,
+          })
+        }
+      >
+        <PlaylistImage source={{ uri: item?.item.images[0].url }} />
+        <PlaylistData>
+          <PlaylistTitle>{item?.item.name}</PlaylistTitle>
+          <PlaylistDescription>{item?.item.description}</PlaylistDescription>
+        </PlaylistData>
+      </PlaylistButton>
     </PlaylistList>
   )
 
